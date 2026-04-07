65a59cdca9c39edd808e82256530ca67e066596d
Naz <ndpm13@ch-naseem.com>
2026-04-07 14:33:18 +0100

    feat: use bindgen to configure & generate bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![allow(
+    non_upper_case_globals,
+    non_camel_case_types,
+    non_snake_case,
+    unnecessary_transmutes
+)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
